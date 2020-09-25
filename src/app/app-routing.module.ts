6447a6c5759f58eeff56f4f2b8dade23dd5e4a15
Paulino Valladares Justo <pvallaj@gmail.com>
2020-09-24 20:17:41 -0500

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConsultaComponent } from './seccion1/consulta/consulta.component';
+import { VideoComponent } from './seccion2/video/video.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'consulta', component:ConsultaComponent},
+  {path:'videos', component:VideoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// MAterial
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+//bootstrap 10
+import {NgbPaginationModule, NgbAlertModule, NgbNav} from '@ng-bootstrap/ng-bootstrap';
+
+//local
+import {MatChipsModule} from '@angular/material/chips';
+import { ConsultaComponent } from './seccion1/consulta/consulta.component';
+import { VideoComponent } from './seccion2/video/video.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
