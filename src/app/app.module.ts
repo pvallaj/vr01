e330d91fff861353b09175f101496b831a97a1e0
Paulino Valladares Justo <pvallaj@gmail.com>
2020-09-25 08:06:19 -0500

    parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 // MAterial
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from "@angular/material/toolbar";
