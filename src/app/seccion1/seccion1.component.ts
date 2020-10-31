5d9a462fdc8975069dae0842d802795a9119fc9c
Paulino Valladares Justo <pvallaj@gmail.com>
2020-10-31 09:23:27 -0600

    preliminares en sermones y categorias en narrativas

--- src/app/seccion1/seccion1.component.ts
+++ src/app/seccion1/seccion1.component.ts
@@ -1,16 +1,124 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormControl} from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+import { ConexionService } from '../servicios/Conexion.service';
+import { MnsjDetalleComponent } from '../seccion1/mnsj-detalle/mnsj-detalle.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-seccion1',
   templateUrl: './seccion1.component.html',
   styleUrls: ['./seccion1.component.css']
 })
 export class Seccion1Component implements OnInit {
-  catalogo: any[]=[];
-  constructor() { }
+  public listaResultado:any[]=[];
+  //autocomplete
+  acAutores = new FormControl();
+  listaAutores:string[]=[];
+  listaAutoresObras:any[]=[];
+  listaObras: any[]=[];
+  listaCategorias: any[]=[];
 
+
+  despResultado:string='block';
+  despDetalle:string='none';
+
+  frm:FormGroup;
+
+  //paginación
+  pidx:number=0; //Número de página.
+  ptam:number=10; //tamaño de la pagina
+
+  //seleccionar elemento
+  idxSeleccionado:number=0;
+
+  //Cargando datos.
+  estaCargando:boolean= false;
+
+  constructor(private cnx:ConexionService,
+     public dialog: MatDialog,
+     private fb:FormBuilder) { }
+  public textConsulta:string;
   ngOnInit(): void {
-   
+    this.crearForma();
+    
+
+
+    this.cnx.narrativas(null, 'consulta catalogo base').subscribe(
+      (datos)=>{
+        this.listaAutores=datos['resultado'].autores;
+        this.listaAutoresObras=datos['resultado'].obras;
+        this.listaCategorias=datos['resultado'].clasificacion;
+      console.log(datos);
+    },
+    (error)=>{
+      console.log("error al cargar a los autores");
+      console.log(error);
+    })
+  }
+  
+  autorSeleccionado(sel){
+    console.log("seleccionado: ");
+    console.log(sel);
+    this.listaObras=this.listaAutoresObras.filter(elm=>elm.autor==sel.value);
+    console.log(this.listaObras);
+  }
+
+  consulta(){
+    this.estaCargando=true;
+    let p={
+      autor: this.frm.value.autor,
+      obra: this.frm.value.obra,
+      desde:  this.pidx*this.ptam,
+      pagtam: this.ptam
+    };
+
+    let temp:any[]=[];
+    this.idxSeleccionado=0;
+
+    this.cnx.narrativas(p, 'consulta narrativas')
+    .subscribe(
+      (data)=>{
+        let idx=1;
+        temp=data['resultado'];
+        this.listaResultado=data['resultado'];
+        console.log(this.listaResultado);
+        this.estaCargando=false;
+      },
+    (error)=>{
+        console.log('No se logro la conexión');
+        console.error(error);
+        this.estaCargando=false;
+      }
+    )
+  }
+  
+  
+  cambiar(){
+    if(this.despResultado=='block'){
+      this.despResultado='none';
+      this.despDetalle='block';
+    }else{
+      this.despResultado='block';
+      this.despDetalle='none';
+    }
+  }
+
+
+  crearForma(){
+    this.frm=this.fb.group({
+      //valor inicial, validaciones sincronas, validaciones asincronas
+      autor:['' ],
+      obra:['' ],
+      categoria:['']
+    });
+  }
+  cambioAutor(){
+    this.listaResultado=[];
   }
 
 }
+
--- src/app/seccion2/cons-det-sermon/cons-det-sermon.component.ts
+++ src/app/seccion2/cons-det-sermon/cons-det-sermon.component.ts
@@ -1,4 +1,6 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit,Input, OnChanges} from '@angular/core';
+import { ConexionService } from '../../servicios/Conexion.service';
+import { MatDialog } from '@angular/material/dialog';
 
 export interface Parametros {
   nombre: string;
@@ -10,11 +12,43 @@ export interface Parametros {
   templateUrl: './cons-det-sermon.component.html',
   styleUrls: ['./cons-det-sermon.component.css']
 })
-export class ConsDetSermonComponent implements OnInit {
+export class ConsDetSermonComponent implements OnInit, OnChanges {
 
-  constructor() {}
+  @Input() id_sermon:number=0;
+  sermon:any=[];
+  libro:any=[];
+  preliminares:any=[];
+  estaCargando:boolean=false;
+
+  constructor(private cnx:ConexionService,
+    public dialog: MatDialog,) {}
 
   ngOnInit(): void {
   }
   
+  ngOnChanges(changes) {
+    console.log("cambio el parametro.....");
+    console.log(changes);
+    if(this.id_sermon==0) return;
+    //consultando detalle del sermon
+    this.cnx.sermones({id_sermon:this.id_sermon}, 'consulta detalle sermon')
+    .subscribe(
+      (data)=>{
+        let idx=1;
+        let temp=data['resultado'];
+        this.sermon=temp.sermon[0];
+        this.libro=temp.libro[0];
+        this.preliminares=temp.preliminares;
+        console.log(temp);
+        this.estaCargando=false;
+      },
+    (error)=>{
+        console.log('No se logro la conexión');
+        console.error(error);
+        this.estaCargando=false;
+      }
+    )
+
+
+  }
 }
