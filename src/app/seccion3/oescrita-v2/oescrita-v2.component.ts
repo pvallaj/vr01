930d6fd3e0b168bf21d736994b6ba167d5845b8b
Paulino Valladares Justo <pvallaj@gmail.com>
2021-02-05 08:01:16 -0600

    Siglo XVI y descripción de herramientas

--- src/app/seccion3/oescrita-v2/oescrita-v2.component.ts
+++ src/app/seccion3/oescrita-v2/oescrita-v2.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ConexionService } from '../../servicios/Conexion.service';
+import { SplitComponent, SplitAreaDirective } from 'angular-split'
+
 
 @Component({
   selector: 'app-oescrita-v2',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OEscritaV2Component implements OnInit {
 
-  constructor() { }
+  @ViewChild('split') split: SplitComponent
+  @ViewChild('area1') area1: SplitAreaDirective
+  @ViewChild('area2') area2: SplitAreaDirective
+
+  public estaCargando=false;
+  public resultado:any[]=null;
+  public imagenes:any[]=null;
+
+  public regsCapitulo:any[]=null;
+  public portada:any=null;
+
+  public om_estructura=true;
+  public om_detalle=true;
+  public om_lista=true;
+  public ctrlVisible=false;
+
+  public elementoSeleccionado:any;
+  public capituloSeleccionado=null;
+
+  constructor(private cnx:ConexionService) { }
 
   ngOnInit(): void {
+    this.estaCargando=true;
+    this.cnx.novohisp({tomo:'SXVI'}, 'consulta estructura x tomo').subscribe(
+      (datos) => {
+        this.resultado=datos['resultado'].estructura;
+        this.imagenes=datos['resultado'].imagenes;
+    },(error) => {
+      console.log('error al cargar a los autores');
+      console.log(error);
+    });
+  }
+
+  public verCapitulo(etiquetas:string){
+    
+    let listae=etiquetas.split(",");
+    listae=listae.filter(e=>e.trim()!='estructura');
+    this.capituloSeleccionado=listae.join(",");
+
+    this.cnx.novohisp({capitulo:this.capituloSeleccionado}, 'consulta capitulo tomo').subscribe(
+      (datos) => {
+        this.regsCapitulo=datos['resultado'].capitulo;
+        this.elementoSeleccionado=this.regsCapitulo[0];
+    },(error) => {
+      console.log('error al cargar a los autores');
+      console.log(error);
+    });
+  }
+
+  public regresar(){
+    this.capituloSeleccionado=null;
+    this.elementoSeleccionado=null;
   }
 
 }
